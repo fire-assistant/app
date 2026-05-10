7483e8326fb08568cce3aee2c03fbe139ca8e38c
carrotcakehope <carrotcakehpe@gmail.com>
2026-05-11 00:00:26 +0900

    update: reset-intro.bat calls deploy automatically

--- reset-intro.bat
+++ reset-intro.bat
@@ -3,9 +3,4 @@ cd /d "%~dp0"
 
 powershell -NoProfile -Command "$f='app.js'; $c=[System.IO.File]::ReadAllText($f); $m=[regex]::Match($c,'introVideoSeen_v(\d+)'); $v=[int]$m.Groups[1].Value; $n=$v+1; $c=$c -replace 'introVideoSeen_v\d+',('introVideoSeen_v'+$n); [System.IO.File]::WriteAllText((Resolve-Path $f).Path,$c); Write-Host ('v'+$v+' to v'+$n)"
 
-git add app.js
-git commit -m "update: reset intro video"
-git push
-
-echo Done! Video will replay for all visitors in 1-2 minutes.
-pause
+call "%~dp0웹배포.bat"
